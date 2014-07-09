c3df7f7a27895788fc15345c5bb677110cb7877e
Håkan Rosenhorn <hrosenhorn@ea.com>
2014-07-09 23:25:04 +0200

    Added handler for obstacles loaded from map and that is in charge of adding and removing from stage

--- src/ObstacleManager.js
+++ src/ObstacleManager.js
@@ -1,5 +1,32 @@
-function ObstacleManager() {
+var _ = require("underscore");
+var MapManager = require("./MapManager");
 
+function ObstacleManager(stage, gameContext) {
+    this.stage = stage;
+    this.gameContext = gameContext;
+    this.obstacles = [];
+    this.viewportX = 0;
+
+    this.mapManager = new MapManager(this.gameContext);
+    this.mapManager.loadLevel();
+    this.obstacles = this.mapManager.getObstacles();
 }
+
+ObstacleManager.prototype.addObstacle = function(obstacle) {
+    this.obstacles.push(obstacle);
+};
+
+ObstacleManager.prototype.setViewportX = function(viewportX) {
+    this.viewportX = viewportX;
+    this.updateObstacles();
+};
+
+// Future improvement would be to split obstacles in sections as a sliding window
+// to avoid checking all entries for visibility
+ObstacleManager.prototype.updateObstacles = function () {
+    _.each(this.obstacles, function (obstacle) {
+        // TODO
+    });
+};
 
 module.exports = ObstacleManager;
